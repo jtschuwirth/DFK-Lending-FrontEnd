a130c687a45718e6c7fa82d516614312a969e20f
jtschuwirth <jtschuwirth@uc.cl>
2022-02-17 10:21:06 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Container from "react-bootstrap/Container";
+
+
+function App() {
+    return(
+        <div className="App">
+            <Header />
+            <Main />
+        </div>
+    )
+}
+
+export default App;
